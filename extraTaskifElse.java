a4c856142e860432456a3eee28a1e34e5589fa96
Evremk <80709842+Evremk@users.noreply.github.com>
2021-03-31 21:02:09 +0200

    Add files via upload

---
+++ extraTaskifElse.java
@@ -0,0 +1,34 @@
+package IfElse;
+
+public class extraTaskifElse {
+
+	public static void main(String[] args) {
+		
+		//MY Approach (it does not work if they are equal? Dont know where is the mistake?
+				  
+
+				int n1=1;
+				int n2=10;
+				int n3=1000;
+		
+		         if(n1>n2) {
+		        	 
+				 if(n1>n3) {
+						System.out.println("Number 1: "+n1+" is the greatest number.");
+			 		}}
+				else if(n2>n1) {
+					if(n2>n3) {
+						System.out.println("Number 2: "+n2+" is the greatest number.");
+					}}
+				else if(n3>n1) {
+					if(n3>n2) {
+						System.out.println("Number 3: "+n3+" is the greatest number.");
+					}}
+				else{
+						System.out.println("The numbers "+n1+" "+n2+" "+n3+" are equal.");	
+					
+				
+				}}
+
+				}
+		         
